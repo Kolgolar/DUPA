2cc7746fe7ea1aaab5d45545853094d665b8ea76
Kolgolar <62188763+Kolgolar@users.noreply.github.com>
2024-09-14 00:18:02 +0300

    Forcing character line, removed excess params from the lite line node

--- LineNodeLite.gd
+++ LineNodeLite.gd
@@ -2,55 +2,32 @@ extends DefaultNode
 
 class_name LineNodeLite
 
-onready var text = $HBoxContainer/MainColumn/Text/Text
 onready var localization_id = $HBoxContainer/MainColumn/Text/LocalizationLine
-onready var choice_name = $HBoxContainer/MainColumn/Choice/ChoiceName
-onready var custom_char_name = $HBoxContainer/MainColumn/Character/CustomCharName
-onready var character_drop = $HBoxContainer/MainColumn/Character/CharacterDrop
+onready var is_player_line = $HBoxContainer/MainColumn/IsPlayer/IsPlayerLine
 
-var characters = [
-	"Player",
-	"Char1",
-	"Char2",
-	"Char3",
-	"Char4",
-	"Char5",
-	"Char6",
-	"Char7",
-	"Char8",
-]
 
 
 func _ready():
 	type = "LINE"
-	var char_index = 0
-	for ch in characters:
-		character_drop.add_item(ch, char_index)
-		char_index += 1 
 
 
 func set_data(graph_edit : GraphEdit, data : Dictionary, id_name : String) -> void:
-	character_drop.text = data["character"]
-	text.text = data["text"]
-	if "choice_name" in data:
-		choice_name.text = data["choice_name"]
 	if "localization_id" in data:
 		localization_id.text = data["localization_id"]
-	if "custom_char_name" in data:
-		custom_char_name.text = data["custom_char_name"]
+	if "is_player" in data:
+		is_player_line.pressed = data["is_player"]
 
 
 func gen_data(graph_edit : GraphEdit) -> Dictionary:
 	var data := {}
 	data["go_to"] = []
-	data["character"] = character_drop.text
-	data["text"] = text.text
-	if not choice_name.text.empty():
-		data["choice_name"] = choice_name.text
+	data["is_player"] = is_player_line.pressed
 	if not localization_id.text.empty():
 		data["localization_id"] = localization_id.text
-	if not custom_char_name.text.empty():
-		data["custom_char_name"] = custom_char_name.text
 	
 	data["go_to"] = _arrange_go_to(graph_edit)
 	return data
+
+
+func _on_IsPlayerLine_toggled(button_pressed):
+	pass # Replace with function body.
